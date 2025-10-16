5a4997361bb4ae7dc2c549aad9e9fb8e7d624f55
Yuyang Wu <wu839@wisc.edu>
2025-10-15 21:27:44 -0500

    update for mean.sh script

--- mean.sh
+++ mean.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+  echo "usage: $0 <column> [file.csv]" 1>&2
+  exit 1
+fi
+
+
+col=$1
+if [[ -n "$2" ]]; then
+  file=$2
+else
+  file=/dev/stdin
+fi
+
+
+cat "$file" | cut -d',' -f "$col" | tail -n +2 | {
+
+    sum=0
+    n=0
+
+    while read x; do
+	sum=$(echo "$sum + $x" | bc)
+	n=$((n+1))
+    done
+
+    mean=$(echo "scale=2; $sum / $n" | bc)
+    echo "$mean"
+}
